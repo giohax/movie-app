0c1c701f45ed9b1068db50611be86d31d73e7d48
giohax <giovannifgonzaga@gmail.com>
2022-11-23 07:43:27 -0600

    implement search by title

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 function App() {
     const [movies, setMovies] = useState<Movie[] | null>(null);
+    const [searchInput, setSearchInput] = useState<string>("");
 
     const URL = "https://code-challenge.spectrumtoolbox.com/api/movies";
 
@@ -9,6 +10,10 @@ function App() {
         id: string;
         title: string;
         genres: string[];
+    };
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchInput(e.target.value);
     };
 
     useEffect(() => {
@@ -22,12 +27,33 @@ function App() {
             .then((data) => setMovies(data.data));
     }, []);
 
+    console.log(
+        movies?.filter((movie) =>
+            movie.title.toLowerCase().includes(searchInput)
+        )
+    );
+
     return (
         <div className="App">
+            <input type="text" value={searchInput} onChange={handleSearch} />
+
             <ul>
-                {movies?.map((movie) => (
-                    <li key={movie.id}>{movie.title}</li>
-                ))}
+                {movies &&
+                    movies
+                        .filter((movie) =>
+                            movie.title.toLowerCase().includes(searchInput)
+                        )
+                        .map((movie) => (
+                            <li key={movie.id}>
+                                <h1>Title: {movie.title}</h1>
+                                <div>
+                                    Genres:{" "}
+                                    {movie.genres.map((genre) => (
+                                        <p key={genre}>{genre}</p>
+                                    ))}
+                                </div>
+                            </li>
+                        ))}
             </ul>
         </div>
     );
